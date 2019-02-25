05024012fb811675e3f7645a8c46eac1a8f0933f
somessii <somessii@163.com>
2019-02-25 10:28:59 +0800

    demo 0225

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel branchs
+                    Laravel branch
                 </div>
 
                 <div class="links">
