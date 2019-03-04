f12e65d3ff350ab334317ad7b727a117d4dfccbd
somessii <somessii@163.com>
2019-03-04 09:23:29 +0800

    3-04

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel master
+                    Laravel masterss
                 </div>
 
                 <div class="links">
