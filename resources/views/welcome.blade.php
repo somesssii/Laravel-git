5e47d63f628db7665bb53d324c4f7689d55ea08a
somessii <somessii@163.com>
2019-03-18 14:08:51 +0800

    branch 0318

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel branchss
+                    Laravel branchss 0318
                 </div>
 
                 <div class="links">
